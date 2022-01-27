959002903930f3c041223aec7f7cd073be1059f1
Miguel <95565345+GreedyBreedy@users.noreply.github.com>
2022-01-27 10:43:42 +0200

    Add files via upload

--- SELECT statement idea.sql
+++ SELECT statement idea.sql
@@ -1,3 +1,9 @@
-SELECT X
-FROM SOMEWHERE
-WHERE a certain condition should EXIST
+CREATE TABLE EMPLOYEE(
+    ID int primary KEY,
+    Name_of_Employee varchar(30),
+    Age int
+);
+INSERT INTO EMPLOYEE VALUES(2020011111,"El Wardi",30);
+SELECT Age
+FROM EOMPLOYEE as E 
+WHERE E.Name_of_employee="El Wardi";
